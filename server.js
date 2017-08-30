0b0477af14d7dc5d6a5bea1a39e323fa318f59c6
tomeraro <tomer_aronovsky@hotmail.com>
2017-08-31 01:17:53 +0300

    x

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.listen(process.env.PORT  || 8080, function() {
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://travel-guru-app.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
