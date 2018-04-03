64f4055d09bf1d25d5784859c6a6bf0763d5a9ec
tomeraro <tomer_aronovsky@hotmail.com>
2018-04-03 20:48:12 +0300

    update server

--- db/equipmentQueries.js
+++ db/equipmentQueries.js
@@ -7,11 +7,13 @@ var uniqBy = require('lodash/uniqBy');
 
 
 exports.getEquipmentBySearchDetails = function(data) {
-  let generalEquipments = exports.getAllGeneralEquipment(data);
+  let generalEquipments = exports.getGeneralEquipment(data);
   let electricEquipment = exports.getElectricEquipment(data);
   let tualeticEquipment = exports.getTualeticEquipment(data);
   let clothingEquipment = exports.getClothingEquipment(data);
-  return Promise.all([generalEquipments, electricEquipment, tualeticEquipment, clothingEquipment])
+  let leisureAndOtherEquipment = exports.getLeisureAndOtherEquipment(data);
+  let medicalEquipment = exports.getAllMedicalEquipment(data);
+  return Promise.all([generalEquipments, electricEquipment, tualeticEquipment, clothingEquipment, leisureAndOtherEquipment, medicalEquipment])
     .then(data => flatten(data));
 }
 
@@ -29,12 +31,32 @@ exports.getEquipmentByEquipmentCategoryAndSeason = function(equipmentCategoryId,
   });
 };
 
+exports.getEquipmentByEquipmentCategoryAndCountry = function(equipmentCategoryId, countryId) {
+  return new Promise(function(resolve, reject) {
+    Equipment.find({ equipmentCategoryId: equipmentCategoryId, countryIds: countryId, isForAll: false })
+    .exec((err, equipments) => resolve(equipments));
+  });
+};
+
 // ******* GENERAL EQUIPMENT ********* //
+exports.getGeneralEquipment = function(data) {
+  let generalEquipmentCategoryId = '59d8e609734d1d18c95c8396';
+  let generalEquipmentForAllPromise = exports.getAllGeneralEquipment();
+  let generalEquipmentByTravelTypePromise = exports.getEquipmentByEquipmentCategoryAndTravelType(generalEquipmentCategoryId, data.travelTypeId);
+  let generalEquipmentByCountryPromise = exports.getEquipmentByEquipmentCategoryAndCountry(generalEquipmentCategoryId, data.countryId);
+  return Promise.all([generalEquipmentForAllPromise, generalEquipmentByTravelTypePromise, generalEquipmentByCountryPromise]).then(data => {
+    let generalEquipmentForAll = data[0];
+    let generalEquipmentByTravelType = data[1];
+    let generalEquipmentByCountry = data[2];
+    let allFinalEquipment = concat(generalEquipmentForAll, generalEquipmentByTravelType, generalEquipmentByCountry);
+    return new Promise((resolve, reject) => resolve(uniqBy(allFinalEquipment, '_id')));
+  }); 
+}
+
 exports.getAllGeneralEquipment = function(data) {
   let generalEquipmentCategoryId = '59d8e609734d1d18c95c8396';
   return new Promise(function(resolve, reject) {
-    Equipment.find({ $or: [{ equipmentCategoryId: generalEquipmentCategoryId, isForAll: true }, 
-    {equipmentCategoryId: generalEquipmentCategoryId, countryIds: data.countryId, isForAll: false }]})
+    Equipment.find({ equipmentCategoryId: generalEquipmentCategoryId, isForAll: true })
     .exec((err, generalEquipments) => resolve(generalEquipments));
   });
 };
@@ -103,3 +125,49 @@ exports.getClothingEquipment = function(data) {
       return new Promise((resolve, reject) => resolve(uniqBy(allFinalEquipment, '_id')));
   });
 }; 
+
+
+// ******* LEISURE AND OTHER EQUIPMENT ********* //
+exports.getAllLeisureAndOtherEquipment = function() {
+  let leisureAndOtherEquipmentCategoryId = '5a85c759c9c0a1648855665a';
+  return new Promise(function(resolve, reject) {
+    Equipment.find({ equipmentCategoryId: leisureAndOtherEquipmentCategoryId, isForAll: true })
+    .exec((err, leisureEquipments) => resolve(leisureEquipments));
+  });
+};
+
+exports.getLeisureAndOtherEquipment = function(data) {
+  let leisureAndOtherEquipmentCategoryId = '5a85c759c9c0a1648855665a';
+  let leisureAndOtherEquipmentsForAllPromise = exports.getAllLeisureAndOtherEquipment();
+  let leisureAndOtherEquipmentsByTravelTypePromise = exports.getEquipmentByEquipmentCategoryAndTravelType(leisureAndOtherEquipmentCategoryId, data.travelTypeId);
+  let leisureAndOtherEquipmentsBySeasonPromise = exports.getEquipmentByEquipmentCategoryAndSeason(leisureAndOtherEquipmentCategoryId, data.seasonId);
+  return Promise.all([leisureAndOtherEquipmentsForAllPromise, leisureAndOtherEquipmentsByTravelTypePromise, leisureAndOtherEquipmentsBySeasonPromise])
+    .then(data => {
+      let leisureAndOtherEquipmentsForAll = data[0];
+      let leisureAndOtherEquipmentsByTravelType = data[1];
+      let leisureAndOtherEquipmentsBySeason = data[2];
+      let allFinalEquipment = concat(leisureAndOtherEquipmentsForAll, leisureAndOtherEquipmentsByTravelType, leisureAndOtherEquipmentsBySeason);
+      return new Promise((resolve, reject) => resolve(uniqBy(allFinalEquipment, '_id')));
+  });
+};
+
+
+// ******* MEDICAL EQUIPMENT ********* //
+exports.getAllMedicalEquipment = function() {
+  let medicalEquipmentCategoryId = '5a85c709c9c0a16488556657';
+  return new Promise(function(resolve, reject) {
+    Equipment.find({ equipmentCategoryId: medicalEquipmentCategoryId, isForAll: true })
+    .exec((err, medicalEquipments) => resolve(medicalEquipments));
+  });
+};
+
+exports.getMedicalEquipment = function(data) {
+  let medicalEquipmentCategoryId = '5a85c709c9c0a16488556657';
+  let medicalEquipmentsForAllPromise = exports.getAllMedicalEquipment();
+  return Promise.all([medicalEquipmentsForAllPromise])
+    .then(data => {
+      let medicalEquipmentsForAll = data[0];
+      let allFinalEquipment = concat(medicalEquipmentsForAll, []);
+      return new Promise((resolve, reject) => resolve(uniqBy(allFinalEquipment, '_id')));
+  });
+};
